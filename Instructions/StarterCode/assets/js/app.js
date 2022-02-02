1d7bfcfc2fb1c16d001eaa41b18aa746537c080c
Kendall <kdmccroy@gmail.com>
2022-02-01 23:17:54 -0500

    Creating a SVG group to hold the charts

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,3 +1,21 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right:40,
+    bottom: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Creating a SVG group to hold the charts
+var svg = d3.select("#scatter").append("svg").attr("width", svgWidth).attr("height", svgHeight);
+
+var chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})` );
+
 d3.csv("assets/data/data.csv").then(function(data){
 
     // Loop through data to find poverty and healthcare
@@ -12,6 +30,11 @@ d3.csv("assets/data/data.csv").then(function(data){
     // Create scale for y axis
     var yAxisScale = d3.scaleLinear().domain([0, d3.max(data, d => d.healthcare)]).range([height, 0]);
 
+    // Creating axis functions
     var xAxis = d3.axisBottom(xAxisScale)
     var yAxis = d3.axisLeft(yAxisScale)
+
+    // Adding Axes to the chart
+    chartGroup.append("g").attr("tranform", `translate (0, ${height})`).call(xAxis);
+    chartGroup.append("g").call(yAxis);
 })
