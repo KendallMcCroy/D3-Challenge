3367d7a8d23ef2a2901ccd06837539e867646a12
Kendall <kdmccroy@gmail.com>
2022-02-01 22:28:23 -0500

    Loop through data to find poverty and healthcare

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,3 +1,8 @@
-d3.csv("assets/data/data.csv").then(function(health_data){
+d3.csv("assets/data/data.csv").then(function(data){
 
+    // Loop through data to find poverty and healthcare
+    data.forEach(function(health_data){
+        health_data.poverty = +health_data.poverty;
+        health_data.healthcare = +health_data.healthcare;
+    })
 })
