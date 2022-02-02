67c5898f47b7d72f49fa255a95a33e1325c9e72c
Kendall <kdmccroy@gmail.com>
2022-02-01 22:43:34 -0500

    Create scale for y axis and x axis

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -2,7 +2,17 @@ d3.csv("assets/data/data.csv").then(function(data){
 
     // Loop through data to find poverty and healthcare
     data.forEach(function(health_data){
-        health_data.poverty = +health_data.poverty;
-        health_data.healthcare = +health_data.healthcare;
+        health_data.poverty = +health_data.poverty; // X axis
+        health_data.healthcare = +health_data.healthcare; // Y Axis
     })
+
+    // Create scale for x axis
+    var xAxisScale = d3.scaleLinear()
+        .domain([8, d3.max(data, d => d.poverty)])
+        .range([0, width]);
+
+    // Create scale for y axis
+    var yAxisScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.healthcare)])
+        .range([height, 0]);
 })
